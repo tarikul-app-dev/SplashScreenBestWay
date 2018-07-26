6870c593ec31a00fc76b5b1891dc91d86ca9b76d
Tarikul <tarikul.android@gmail.com>
2018-07-26 16:28:50 +0600

    first update

---
+++ app/src/main/java/planet/it/limited/splashscreenbestway/SplashActivity.java
@@ -0,0 +1,17 @@
+package planet.it.limited.splashscreenbestway;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
